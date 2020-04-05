c87808b7a68e677caa8198c39813a445181ac7d7
Osama Sayed <61942698+Osama-Sayed@users.noreply.github.com>
2020-04-05 22:35:59 +0200

    Add files via upload

---
+++ FAQs/script.js
@@ -0,0 +1,59 @@
+var q1=document.getElementById("a1");
+var b1=document.getElementById("b1");
+function showAndHide1()
+{
+    if(q1.style.display=="none")
+    {q1.style.display="block";b1.value="-"}
+    else
+     { q1.style.display="none";b1.value="+" }  
+}
+
+
+var q2=document.getElementById("a2");
+var b2=document.getElementById("b2");
+function showAndHide2()
+{
+    if(q2.style.display=="none")
+       { q2.style.display="block"; b2.value="-"}
+    else
+   {   q2.style.display="none";b2.value="+"}
+}
+
+var q3=document.getElementById("a3");
+var b3=document.getElementById("b3");
+function showAndHide3()
+{
+    if(q3.style.display=="none")
+       { q3.style.display="block"; b3.value="-"}
+    else
+   {   q3.style.display="none";b3.value="+"}
+}
+var q4=document.getElementById("a4");
+var b4=document.getElementById("b4");
+function showAndHide4()
+{
+    if(q4.style.display=="none")
+       { q4.style.display="block"; b4.value="-"}
+    else
+   {   q4.style.display="none";b4.value="+"}
+}
+
+var q5=document.getElementById("a5");
+var b5=document.getElementById("b5");
+function showAndHide5()
+{
+    if(q5.style.display=="none")
+       { q5.style.display="block"; b5.value="-"}
+    else
+   {   q5.style.display="none";b5.value="+"}
+}
+
+var q6=document.getElementById("a6");
+var b6=document.getElementById("b6");
+function showAndHide6()
+{
+    if(q6.style.display=="none")
+       { q6.style.display="block"; b6.value="-"}
+    else
+   {   q6.style.display="none";b6.value="+"}
+}
